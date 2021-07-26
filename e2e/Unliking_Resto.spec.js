1ca48505d8e459ddc56532b6054f1f6958ccf33c
Achmad Alghifari <31951431+misterquinze@users.noreply.github.com>
2021-07-26 20:54:34 +0700

    split liking resto and unliking resto e2e test

--- e2e/Unliking_Resto.spec.js
+++ e2e/Unliking_Resto.spec.js
@@ -1,7 +1,5 @@
 /* eslint-disable no-undef */
-const assert = require('assert');
-
-Feature('Liking Resto');
+Feature('Unliking Resto');
 
 Before(({ I }) => {
   I.amOnPage('/#/favorite');
@@ -11,18 +9,20 @@ Scenario('checking that the list is empty', ({ I }) => {
   I.dontSeeElement('#restaurant .restaurant-item');
 });
 
-Scenario('liking one resto', async ({ I }) => {
+Scenario('unliking one resto', async ({ I }) => {
   I.amOnPage('/');
   I.seeElement('.restaurant-item__title a');
   I.wait(5);
-  const firstResto = locate('.restaurant-item__title a').first();
-  const firstRestoName = await I.grabTextFrom(firstResto);
-  I.click(firstResto);
-  I.wait(5);
+  I.click(locate('.restaurant-item__title a').first());
   I.seeElement('#favoriteButton');
   I.click('#favoriteButton');
   I.amOnPage('/#/favorite');
-  I.seeElement('.restaurant-item');
-  const likedRestoName = await I.grabTextFrom('.restaurant-item__title a');
-  assert.strictEqual(firstRestoName, likedRestoName);
+  I.seeElement('.restaurant-item__title a');
+  I.wait(7);
+  const firstResto = locate('.restaurant-item__title a').first();
+  I.click(firstResto);
+  I.seeElement('#favoriteButton');
+  I.click('#favoriteButton');
+  I.amOnPage('/#/favorite');
+  I.dontSeeElement('.restaurant-item');
 });
